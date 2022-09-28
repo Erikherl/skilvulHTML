fee3925f2d3c4a7bdc1168c80f115d3acbfe636e
erikherl <erikherl7@gmail.com>
2022-09-28 23:14:07 +0700

    Added css and js

---
+++ index.js
@@ -0,0 +1,85 @@
+angka = 15;
+huruf = "Coba Itu";
+objek = {nama: "Erik", age: 21}
+
+console.log(typeof angka)
+console.log(typeof huruf)
+console.log(typeof objek)
+
+kosong = null;
+boolean = false;
+
+
+
+let Nama = "Frederik";
+
+let Matematika = 4 + 4;
+Matematika++;
+console.log(Matematika % 2)
+
+console.log("apple" == "jeruk")
+console.log(17 > 5)
+
+if(5 == 5 && 6 == 6){}
+
+
+
+
+
+
+if(true){
+    console.log("Happy")
+}else{
+    console.log("Sad")
+}
+
+
+let jumbawa = "Hah?";
+if(jumbawa){
+    console.log("Bjir")
+}else{
+    console.log("Lah?")
+}
+
+
+let clicked = 3;
+
+switch(clicked){
+    case 1:{
+        console.log("Loh kamu disini?"); break;
+    }
+    case 2:{
+        console.log("Loh kamu ngapain?"); break;
+    }
+    case 3:{
+        console.log("Siapa kamu? Kenalan yuk"); break;
+    }
+    default: {console.log("Bisa aja si ojang")}
+}
+
+
+let hadir = true;
+
+hadir ? console.log('Absen aman') : console.log('Ulangi Kelas')
+
+
+console.log(1);
+console.log(2);
+console.log(3);
+console.log(4);
+console.log(5);
+console.log(6);
+console.log(7);
+console.log(8);
+
+
+for(angka = 1; angka <= 8; angka++){
+    console.log(angka)
+}
+
+
+let i = 1
+while (i <= 8){
+    console.log(i);
+    angka++
+}
